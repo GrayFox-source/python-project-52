d81a2b8d84282cbb323362a0733115a31d807ad0
Flexabler <smolnikov2023@list.ru>
2026-09-25 00:29:02 +0500

    Fix-another-2;

--- tasks/forms.py
+++ tasks/forms.py
@@ -1,5 +1,4 @@
 from django import forms
-from django.contrib.auth.models import User
 from .models import Task
 
 class TaskForm(forms.ModelForm):
@@ -28,7 +27,7 @@ class TaskForm(forms.ModelForm):
                 'class': 'w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500'
             }),
             'labels': forms.SelectMultiple(attrs={
-                'class': 'w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500',
-                'size': '5'
+                'size': '5',
+                'class': 'w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500'
             }),
         }
