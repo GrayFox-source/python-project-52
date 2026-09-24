3ee39f54d251e9fe61abe67eef53e72f06d51ee8
Flexabler <smolnikov2023@list.ru>
2026-09-24 21:32:08 +0500

    fix;

--- users/views.py
+++ users/views.py
@@ -26,11 +26,11 @@ class UserCreateView(CreateView):
         messages.success(self.request, 'Пользователь успешно зарегистрирован')
         return super().form_valid(form)
 
-    def form_invalid(self, form):
-        # ВРЕМЕННЫЙ ДЕБАГ: выведет ошибки в логи GitHub Actions
-        print(f"DEBUG FORM ERRORS: {form.errors}", file=sys.stderr)
-        print(f"DEBUG FORM DATA: {self.request.POST}", file=sys.stderr)
-        return super().form_invalid(form)
+    # def form_invalid(self, form):
+    #     # ВРЕМЕННЫЙ ДЕБАГ: выведет ошибки в логи GitHub Actions
+    #     print(f"DEBUG FORM ERRORS: {form.errors}", file=sys.stderr)
+    #     print(f"DEBUG FORM DATA: {self.request.POST}", file=sys.stderr)
+    #     return super().form_invalid(form)
 
 
 class UserUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
