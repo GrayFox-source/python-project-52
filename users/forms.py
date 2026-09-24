5973faa4001024e4a2e87ec88891623dcd9ae650
Flexabler <smolnikov2023@list.ru>
2026-09-24 21:09:01 +0500

    fix user forms;

--- users/forms.py
+++ users/forms.py
@@ -1,16 +1,14 @@
 from django import forms
-from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-
+from django.contrib.auth.forms import UserCreationForm
 
 class CustomUserCreationForm(UserCreationForm):
-    first_name = forms.CharField(label='Имя', required=True)
-    last_name = forms.CharField(label='Фамилия', required=True)
-
     class Meta(UserCreationForm.Meta):
         model = User
         fields = ('first_name', 'last_name', 'username', 'password1', 'password2')
         labels = {
+            'first_name': 'Имя',
+            'last_name': 'Фамилия',
             'username': 'Имя пользователя',
             'password1': 'Пароль',
             'password2': 'Подтверждение пароля',
